c4aa90457202d9bf49a3b7261d0b482cede90315
unknown <shai@mcterano.com>
2018-11-25 14:56:11 +0200

    mongo delete

--- mongo/index.js
+++ mongo/index.js
@@ -7,12 +7,21 @@ const url = "mongodb://127.0.0.1:27017/cars";
 
 MongoClient.connect( url, (err, db) => {
     const collection = db.db('cars').collection('models');
-
-    app.get('/', (req, res) => {
-        collection.find().toArray( (err, docs) => {
-            res.send(docs);
-        } )
-    })
+    app.locals.collection = collection;
 } );
+
+app.get('/', (req, res) => {
+    // collection.find().toArray( (err, docs) => {
+    //     res.send(docs);
+    // } )
+    app.locals.collection.find().toArray( (err, docs) => {
+        res.send(docs);
+    } );
+})
+
+app.delete('/:carName', (req, res) => {
+    app.locals.collection.deleteOne({ "name": req.params.carName }, (err, result) => console.log(result));
+    res.send('ok');
+})
 
 app.listen(8080, () => console.log('Server is running'));
